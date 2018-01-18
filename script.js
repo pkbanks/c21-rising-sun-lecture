d61017b8d5ef8eba6bbdc0b8486c38f1421e6698
PK Banks <pkbanks12@gmail.com>
2018-01-18 11:47:33 -0500

    pulling in data from weather api via ajax and now pulling in sunrise and sunset out of response

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // script.js
+// const weather_api = 'http://api.openweathermap.org/data/2.5/weather?q=miami,us&APPID=5e182bf346c2bb165ff896592d5b9807';
+
+const weather_api = 'http://api.openweathermap.org/data/2.5/weather?q=miami,us&APPID=5e182bf346c2bb165ff896592d5b9807'
+
+window.addEventListener('load', function(){
+
+});
+
+function getWeatherData() {
+  const data = JSON.parse(this.responseText);
+  console.dir(data);
+  
+  const sunrise = data.sys.sunrise;
+  const sunset = data.sys.sunset;
+}
+
+const connection = new XMLHttpRequest();
+
+connection.addEventListener("load", getWeatherData);
+
+connection.open("GET", weather_api);
+connection.send();
+
